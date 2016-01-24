81cbff96781cd6d9dd642d42f055b41955e64edb
Rodrigo Vaccari <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-01-24 14:59:49 -0200

    Corrição na navegação

--- eventex/urls.py
+++ eventex/urls.py
@@ -6,7 +6,7 @@ from eventex.core.views import home
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-    url(r'^inscricao', include('eventex.subscriptions.urls',
+    url(r'^inscricao/', include('eventex.subscriptions.urls',
                                namespace='subscriptions')),
     url(r'^admin/', admin.site.urls),
 ]
