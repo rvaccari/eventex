1c081fc27838c90ff48e1fb91410b12b2b3ac1fc
Rodrigo Vaccari <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-02-22 22:30:44 -0300

    Talk

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -19,6 +19,7 @@ class Speaker(models.Model):
         return r('speaker_detail', slug=self.slug)
 
 
+
 class Contact(models.Model):
     EMAIL = 'E'
     PHONE = 'P'
@@ -38,3 +39,17 @@ class Contact(models.Model):
     def __str__(self):
         return self.value
 
+
+class Talk(models.Model):
+    title = models.CharField('título', max_length=200)
+    start = models.TimeField('início', blank=True, null=True)
+    description = models.TextField('descrição', blank=True)
+    speakers = models.ManyToManyField('Speaker', verbose_name='palestrantes', blank=True)
+
+
+    class Meta:
+        verbose_name_plural = 'palestras'
+        verbose_name = 'palestra'
+
+    def __str__(self):
+        return self.title
