28d978c7303620f881a508ee6df5cd3a5ab4a25f
Rodrigo Vaccari <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-03-16 22:37:07 -0300

    Course como MTI

--- eventex/core/tests/test_view_talk_list.py
+++ eventex/core/tests/test_view_talk_list.py
@@ -11,7 +11,7 @@ class TalkListGet(TestCase):
                                  description='Descrição da palestra.')
 
         c1 = Course.objects.create(title='Título do Curso', start='09:00',
-                                 description='Descrição do curso.', slots=20 )
+                                      description='Descrição do curso.', slots=20)
 
 
         speaker = Speaker.objects.create(name='Rodrigo Vaccari',
@@ -48,7 +48,7 @@ class TalkListGet(TestCase):
                 self.assertContains(self.resp, expected, count)
 
     def test_context(self):
-        variables = ['morning_talks', 'afternoon_talks', 'courses']
+        variables = ['morning_talks', 'afternoon_talks']
 
         for key in variables:
             with self.subTest():
