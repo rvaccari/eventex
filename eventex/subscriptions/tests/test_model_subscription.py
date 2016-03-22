4894fa1b79b3f20c0ac26ed61d503746b858e817
Rodrigo Vaccari <rodrigo@MacBook-Pro-de-Rodrigo.local>
2016-03-22 09:03:37 -0300

    Mixins

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+from django.shortcuts import resolve_url as r
 from django.test import TestCase
 from eventex.subscriptions.models import Subscription
+
 
 class SubscritionModelTest(TestCase):
     def setUp(self):
@@ -26,3 +28,7 @@ class SubscritionModelTest(TestCase):
     def test_paid_default_to_False(self):
         """By default paid must be False."""
         self.assertEqual(False, self.obj.paid)
+
+    def test_get_absolute_url(self):
+        url = r('subscriptions:detail', self.obj.pk)
+        self.assertEqual(url, self.obj.get_absolute_url())
