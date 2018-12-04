40c9e5501e4a131f70a8e7cef16e3feaa4009578
Rodrigo Vaccari <rvaccari@gmail.com>
2018-12-04 14:56:46 -0200

    adiciona str no model Cancelado

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -74,3 +74,6 @@ class Course(Talk):
 class Cancelado(models.Model):
     created_at = models.DateTimeField('criado em', auto_now_add=True)
     resposta = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.resposta
